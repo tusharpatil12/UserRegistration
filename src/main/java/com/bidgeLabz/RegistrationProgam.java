94258dd3d589580eb6936e799209bfb484fa8039
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 <tusharpatil22700@gmail.com>
2022-11-10 21:49:01 -0800

    welcome

---
+++ src/main/java/com/bidgeLabz/RegistrationProgam.java
@@ -0,0 +1,10 @@
+package com.bidgeLabz;
+
+public class RegistrationProgam {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to User Registration Program");
+
+	}
+
+}
---
+++ src/test/java/com/bidgeLabz/Validation.java
@@ -0,0 +1,5 @@
+package com.bidgeLabz;
+
+public class Validation {
+
+}
